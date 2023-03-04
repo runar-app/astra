8d820db5e5471c3d5701ac8798ac204c8a692945
Alexandr Dmowski <dmowski@yandex.ru>
2023-03-04 02:10:30 +0100

    Fix web view

--- views/Assistant/index.tsx
+++ views/Assistant/index.tsx
@@ -1,39 +1,22 @@
 import * as React from "react";
-import { Text, View, StyleSheet, SafeAreaView, StatusBar, ScrollView } from "react-native";
+import { StyleSheet, StatusBar } from "react-native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { WebView } from "react-native-webview";
+
+const vikingUrl = "https://viking-ai.vercel.app/";
 
 const styles = StyleSheet.create({
   webView: {
     marginTop: 20,
     marginBottom: 20,
     width: "100%",
-    height: "100%",
-  },
-  container: {
-    flex: 1,
-    paddingTop: StatusBar.currentHeight,
-    height: "100%",
-    backgroundColor: "red",
-  },
-  scrollView: {
-    backgroundColor: "pink",
-    height: "100%",
-  },
-  view: {
-    height: "100%",
+    height: "101%",
+    backgroundColor: "#000000",
   },
 });
 
 function AssistantScreen() {
-  return (
-    <WebView
-      scrollEnabled={false}
-      textZoom={100}
-      source={{ uri: "https://chat-run.vercel.app/" }}
-      style={styles.webView}
-    />
-  );
+  return <WebView textZoom={100} source={{ uri: vikingUrl }} style={styles.webView} />;
 }
 
 const AssistantStack = createNativeStackNavigator();
