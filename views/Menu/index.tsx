eca144946ba7fb5bc39381b5c5e539e321c4b4aa
Alexandr Dmowski <dmowski@yandex.ru>
2023-03-04 02:19:45 +0100

    About screen

--- views/Menu/index.tsx
+++ views/Menu/index.tsx
@@ -1,6 +1,15 @@
 import * as React from "react";
-import { Button, Text, View } from "react-native";
+import { Button, Text, View, StyleSheet } from "react-native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const styles = StyleSheet.create({
+  aboutText: {
+    fontSize: 15,
+    color: "#ffffff",
+    width: "90%",
+    padding: 6,
+  },
+});
 
 function SettingsScreen() {
   return (
@@ -20,8 +29,19 @@ function FavoriteScreen() {
 
 function AboutScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Hi it is About!</Text>
+    <View style={{ flex: 1, justifyContent: "flex-start", alignItems: "center", paddingTop: 15 }}>
+      <Text style={styles.aboutText}>Версия приложения 1.0</Text>
+      <Text style={styles.aboutText}>
+        Runar — это приложение для гадания на скандинавских рунах и изучения скандинавской мифологии
+        и сказок. Содержит 8 видов рунных раскладов, толкования рун. В разделе Библиотека вы можете
+        почитать скандинавские саги и сказки.
+      </Text>
+      <Text style={styles.aboutText}>
+        С разрешения правообладателей, в приложении использованы следующие музыкальные композиции: -
+        Лёдъ (использованы композиции - «Черная Ладья», «Мать моя сказала»),
+        https://lyod1.bandcamp.com/releases - Danheim (использованы композиции - «Runar», «Kala»),
+        https://danheimmusic.com/
+      </Text>
     </View>
   );
 }
