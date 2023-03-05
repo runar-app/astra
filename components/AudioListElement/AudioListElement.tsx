90d331e78f6f026b36b94c5f0fa40606d79ad320
Alexandr Dmowski <dmowski@yandex.ru>
2023-03-05 02:04:29 +0100

    AudioList

---
+++ components/AudioListElement/AudioListElement.tsx
@@ -0,0 +1,29 @@
+import { Text, View, StyleSheet } from "react-native";
+import { Colors, FontSizes } from "../../commonStyle";
+import React from "react";
+import { BaseText } from "../Typography/BaseText";
+import { AudioBook } from "../../types/audios";
+
+interface AudioListElementProps {
+  audioData: AudioBook;
+  color?: string;
+}
+
+export const AudioListElement = ({ audioData }: AudioListElementProps) => {
+  return (
+    <View style={styles.listItem}>
+      <BaseText>{audioData.title}</BaseText>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  listItem: {
+    paddingTop: 15,
+    paddingBottom: 15,
+    paddingLeft: 15,
+    paddingRight: 15,
+    borderBottomWidth: 1,
+    borderBottomColor: "#222",
+  },
+});
--- views/Library/index.tsx
+++ views/Library/index.tsx
@@ -1,11 +1,12 @@
 import * as React from "react";
-import { ActivityIndicator, Text, View } from "react-native";
+import { ActivityIndicator, FlatList, Text, View } from "react-native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { AudioBook } from "../../types/audios";
 import { getListOfAudios } from "../../services/audios";
 import { BaseText } from "../../components/Typography/BaseText";
 import { Colors } from "../../commonStyle";
 import { SmallLoaderPage } from "../../components/Loader/SmallLoaderPage";
+import { AudioListElement } from "../../components/AudioListElement/AudioListElement";
 
 function LibraryScreen() {
   const [audios, setAudios] = React.useState<AudioBook[]>([]);
@@ -25,7 +26,23 @@ function LibraryScreen() {
     return <SmallLoaderPage loadingTextMessage="Loading data..." />;
   }
 
-  return <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}></View>;
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        padding: 0,
+        alignItems: "center",
+      }}
+    >
+      <FlatList
+        data={audios}
+        renderItem={({ item }) => <AudioListElement audioData={item} />}
+        keyExtractor={(item) => item._id}
+        style={{ width: "100%" }}
+      />
+    </View>
+  );
 }
 
 const LibraryStack = createNativeStackNavigator();
