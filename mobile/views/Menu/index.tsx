00bcea6036122621e1180e8b61c93c1ec489a4eb
Alexandr Dmowski <dmowski@yandex.ru>
2023-03-07 03:56:43 +0100

    fix menu

--- mobile/views/Menu/index.tsx
+++ mobile/views/Menu/index.tsx
@@ -4,49 +4,36 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Background } from "../../components/Background/Background";
 import { MenuScreen } from "./Menu";
 import { UIMessage } from "../../data/messages";
+import { BaseText } from "../../components/Typography/BaseText";
+import { SmallText } from "../../components/Typography/SmallText";
 
 const styles = StyleSheet.create({
-  aboutText: {
-    fontSize: 15,
-    color: "#ffffff",
-    width: "90%",
-    padding: 12,
+  page: {
+    flexDirection: "column",
+    gap: 20,
+    padding: 30,
+    width: "94%",
+    height: "100%",
   },
 });
 
-function SettingsScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Hi it is Settings!</Text>
-    </View>
-  );
-}
-
-function FavoriteScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Hi it is Favorite!</Text>
-    </View>
-  );
-}
 function AboutScreen() {
   return (
-    <ScrollView>
-      <Background>
-        <Text style={styles.aboutText}>Версия приложения 1.0</Text>
-        <Text style={styles.aboutText}>
-          Runar — это приложение для гадания на скандинавских рунах и изучения скандинавской
-          мифологии и сказок. Содержит 8 видов рунных раскладов, толкования рун. В разделе
-          Библиотека вы можете почитать скандинавские саги и сказки.
-        </Text>
-        <Text style={styles.aboutText}>
-          С разрешения правообладателей, в приложении использованы следующие музыкальные композиции:
-          - Лёдъ (использованы композиции - «Черная Ладья», «Мать моя сказала»),
-          https://lyod1.bandcamp.com/releases - Danheim (использованы композиции - «Runar», «Kala»),
-          https://danheimmusic.com/
-        </Text>
-      </Background>
-    </ScrollView>
+    <Background>
+      <ScrollView>
+        <View style={styles.page}>
+          <BaseText>Версия приложения 1.0</BaseText>
+          <SmallText>
+            Runar Astra — это приложение с викинг-помощником, чат-ботом ассистентом, который может
+            ответить на любой вопрос о скандинавской мифологии.
+          </SmallText>
+          <SmallText>Помощник обладает огромным знанием о богах, героях и сказаниях. </SmallText>
+          <SmallText>
+            Приложение также содержит аудиосказки о битвах богов и приключениях викингов.{" "}
+          </SmallText>
+        </View>
+      </ScrollView>
+    </Background>
   );
 }
 
@@ -62,8 +49,6 @@ export function MenuStackScreen() {
         }}
         component={MenuScreen}
       />
-      <MenuStack.Screen name="Settings" component={SettingsScreen} />
-      <MenuStack.Screen name="Favorite" component={FavoriteScreen} />
       <MenuStack.Screen name="About" component={AboutScreen} />
     </MenuStack.Navigator>
   );
